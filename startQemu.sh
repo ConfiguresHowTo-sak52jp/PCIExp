ceef649a126a9ebf88ced63a96bed19e75281cec
sak52jp <charan.49jp@gmail.com>
2021-08-03 11:47:36 +0900

    起動スクリプトからコメントアウト部分を削除

--- startQemu.sh
+++ startQemu.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# ./qemu/aarch64-softmmu/qemu-system-aarch64 \
-#     -M arm-generic-fdt-7series -machine linux=on \
-#     -serial /dev/null -serial mon:stdio -display none \
-#     -kernel rsrc/linux-xlnx/arch/arm/boot/uImage \
-#     -dtb rsrc/linux-xlnx/arch/arm/boot/dts/zynq-zc706.dtb \
-#     -sd rsrc/rootfs.img -append 'root=/dev/mmcblk0 rw rootwait'
-
 qemu-6.0.0/build/qemu-system-aarch64 \
     -M virt -cpu cortex-a53 -nographic -smp 1 -kernel Image \
     -append "rootwait root=/dev/vda console=ttyAMA0" \
